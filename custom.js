addaf98a9490a7b858a78a9d5c1ef0bdf7a4fc10
Md Saiful Islam <saiful.sgd@gmail.com>
2024-11-15 18:33:36 +0600

    portfolio-1
    demo web site without java script

---
+++ custom.js
@@ -0,0 +1,14 @@
+         "use strict"
+
+const person= {
+    firstName : 'saiful',
+    lastName : 'Islam',
+    age : 43,
+    hobies : ['music','adieo','video','travel'],
+    adress: {
+        city: 'rangpur',
+        zilla: 'ghoraghat'
+    }
+} 
+
+console.log(person.age);
